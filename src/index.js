cb9b6e62c5208d5b23006b1f36f58361320668c7
Artur Chyży <artur.chyzy@gmail.com>
2017-06-01 15:18:37 +0200

    After section 1 - An Intro to React

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyDEO72E2rqSRyartmI93-T822kcof2aKT4';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Create a new component. This component should produce some HTML
+const App = () => {
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
+};
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component's HTML and put it on the page (in the DOM)
+ReactDOM.render(<App/>, document.querySelector('.container'));
